7a12a9d16fc020d63a4af182b686b74b09749e7a
feras alnowiser <ferasdev@gmail.com>
2022-07-22 23:49:50 +0300

    make the connectViews function to connect the views in the activity_main.xml

--- app/src/main/java/com/example/week05day04hw/MainActivity.kt
+++ app/src/main/java/com/example/week05day04hw/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.week05day04hw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private var drawerLayout:DrawerLayout? = null
+    private var navigationView:NavigationView? = null
+    private var toolbar:Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        connectViews()
+    }
+
+    private fun connectViews() {
+        drawerLayout = findViewById(R.id.drawer_layout_activity_main)
+        navigationView = findViewById(R.id.nav_view_activity_main)
+        toolbar = findViewById(R.id.toolbar_activity_main)
     }
 }
