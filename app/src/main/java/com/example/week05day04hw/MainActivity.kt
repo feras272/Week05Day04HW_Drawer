46954ab268b7cb7119980f61c705f97dbdfd2464
feras alnowiser <ferasdev@gmail.com>
2022-07-23 20:47:58 +0300

    finishing the menu layout in MainActivity.kt by drawableClicks function and setting the actionBar

--- app/src/main/java/com/example/week05day04hw/MainActivity.kt
+++ app/src/main/java/com/example/week05day04hw/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.week05day04hw
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toolbar
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -20,7 +21,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         connectViews()
+
+        setSupportActionBar(toolbar!!)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setupDrawer()
+        drawerClicks()
     }
 
     private fun connectViews() {
@@ -46,4 +51,43 @@ class MainActivity : AppCompatActivity() {
             else -> true
         }
     }
+
+    private fun drawerClicks() {
+        navigationView?.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.camera_import -> {
+                    Toast.makeText(this, "Import button has clicked", Toast.LENGTH_SHORT).show()
+                    drawerLayout?.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.gallery -> {
+                    Toast.makeText(this, "Gallery button has clicked", Toast.LENGTH_SHORT).show()
+                    drawerLayout?.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.subscribe -> {
+                    Toast.makeText(this, "Subscribe button has clicked", Toast.LENGTH_SHORT).show()
+                    drawerLayout?.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.tools -> {
+                    Toast.makeText(this, "Tools button has clicked", Toast.LENGTH_SHORT).show()
+                    drawerLayout?.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.share -> {
+                    Toast.makeText(this, "Share button has clicked", Toast.LENGTH_SHORT).show()
+                    drawerLayout?.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.send -> {
+                    Toast.makeText(this, "Send button has clicked", Toast.LENGTH_SHORT).show()
+                    drawerLayout?.closeDrawer(GravityCompat.START)
+                    true
+                }
+                else -> true
+            }
+        }
+    }
+
 }
