432b609ffaa5865c160097d47d11fd2bca1623f8
feras alnowiser <ferasdev@gmail.com>
2022-07-23 20:31:52 +0300

    setting up the drawer with setupDrawer function and for the top side option

--- app/src/main/java/com/example/week05day04hw/MainActivity.kt
+++ app/src/main/java/com/example/week05day04hw/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.week05day04hw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
@@ -17,11 +20,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         connectViews()
+        setupDrawer()
     }
 
     private fun connectViews() {
         drawerLayout = findViewById(R.id.drawer_layout_activity_main)
         navigationView = findViewById(R.id.nav_view_activity_main)
         toolbar = findViewById(R.id.toolbar_activity_main)
+    }
+
+    private fun setupDrawer() {
+        val toggleDrawer:ActionBarDrawerToggle = ActionBarDrawerToggle(this,
+            drawerLayout, R.string.open, R.string.close)
+
+        drawerLayout?.addDrawerListener(toggleDrawer)
+        toggleDrawer.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            android.R.id.home -> {
+                drawerLayout?.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> true
+        }
     }
 }
