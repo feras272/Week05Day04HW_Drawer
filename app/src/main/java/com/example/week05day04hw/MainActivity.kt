c8619842399b69b67e50a38577f4e6a40bb9ce60
feras alnowiser <ferasdev@gmail.com>
2022-07-24 11:48:53 +0300

    connecting the message button with its message when it clicked, and user image. lastly, trying to fitch the user image to be clicked

--- app/src/main/java/com/example/week05day04hw/MainActivity.kt
+++ app/src/main/java/com/example/week05day04hw/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.week05day04hw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +20,9 @@ class MainActivity : AppCompatActivity() {
     private var drawerLayout:DrawerLayout? = null
     private var navigationView:NavigationView? = null
     private var toolbar:Toolbar? = null
+    private var floatingActionButtonMessage:FloatingActionButton? = null
+    private var imageButton:ImageButton? = null
+    private var constraintLayout:ConstraintLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +34,25 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setupDrawer()
         drawerClicks()
+
+        imageButtonClick()
+
+
+
     }
 
     private fun connectViews() {
         drawerLayout = findViewById(R.id.drawer_layout_activity_main)
         navigationView = findViewById(R.id.nav_view_activity_main)
         toolbar = findViewById(R.id.toolbar_activity_main)
+        floatingActionButtonMessage = findViewById(R.id.fab_message_activity_main)
+//        imageButton = findViewById(R.id.ib_user_header)
+        var cl = layoutInflater.inflate(R.layout.header, null)
+        try {
+            imageButton = cl!!.findViewById(R.id.ib_user_header)
+        } catch (e:Exception) {
+            Toast.makeText(this, "Error happened during retrieving user image", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setupDrawer() {
@@ -88,6 +109,14 @@ class MainActivity : AppCompatActivity() {
                 else -> true
             }
         }
+    }
+
+    private fun imageButtonClick() {
+        imageButton?.setOnClickListener {
+            Toast.makeText(this, "User image has clicked", Toast.LENGTH_SHORT).show()
+            drawerLayout?.closeDrawer(GravityCompat.START)
+        }
+
     }
 
 }
